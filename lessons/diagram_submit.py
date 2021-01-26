b41944e2299ed69a4365e19c455a6d364cb01d60
Seanna Chen <seannaqi@live.unc.edu>
2021-01-26 16:50:18 -0500

    Lesson 1/26/21

---
+++ lessons/diagram_submit.py
@@ -0,0 +1,10 @@
+"""Example diagram problem."""
+
+age: int = int(input("How old are you? "))
+year: int = int(input("What year is it?"))
+age_in_2041: int = 2041 - year + age
+print("In 2041, you'll be " + str(age_in_2041))
+
+age = age + 1  #this redefined age and year from before
+year = year + 1
+print("In " + str(year) + ", you'll be " + str(age))
